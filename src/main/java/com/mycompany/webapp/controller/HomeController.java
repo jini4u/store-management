439eee4a216107f7fd61fe945d027398fdfeec6c
jini4u <108965125+jini4u@users.noreply.github.com>
2022-12-27 09:04:38 +0900

    유진 전날 부분 수정!

--- src/main/java/com/mycompany/webapp/controller/HomeController.java
+++ src/main/java/com/mycompany/webapp/controller/HomeController.java
@@ -24,25 +24,24 @@ public class HomeController {
 		System.out.println(test.test());
 		return "home";
 	}
-<<<<<<< HEAD
+
 	@RequestMapping(value="/centerList")
 	public String loadCenter() {
 		System.out.println("센터조회");
 		return "centerList";
-=======
+	}
 
 	@RequestMapping("/login")
 	public String login() {
 		return "login";
->>>>>>> refs/remotes/origin/master
 	}
 	
-<<<<<<< HEAD
 	@RequestMapping(value="/centerPhoto")
 	public String managePhotoCenter() {
 		System.out.println("사진관리");
 		return "centerPhoto";
-=======
+	}
+	
 	@RequestMapping("/code")
 	public String code() {
 		return "code";
@@ -51,7 +50,6 @@ public class HomeController {
 	@RequestMapping("/statistics")
 	public String statistics() {
 		return "statistics";
->>>>>>> refs/remotes/origin/master
 	}
 }
 
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
 <%@ page contentType="text/html; charset=UTF-8" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<%@ include file="/WEB-INF/views/common/header.jsp" %>
 
 <div class="card m-2">
 	<div class="card-header">
@@ -12,10 +10,3 @@
 		<a href="<c:url value='/centerPhoto'/>">센터 사진 관리</a>
 	</div>
 </div>
-
-<%@ include file="/WEB-INF/views/common/footer.jsp" %>
-=======
-<h1>HOME2.jsp</h1>
->>>>>>> refs/remotes/origin/master
-
-
