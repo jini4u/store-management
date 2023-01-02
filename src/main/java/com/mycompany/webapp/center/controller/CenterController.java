ccd07c03d2ae0bf0f8212195754dd48b6100f2b6
sojung <red8544@gmail.com>
2023-01-02 17:54:46 +0900

    fileInfoVO 추가

--- src/main/java/com/mycompany/webapp/center/controller/CenterController.java
+++ src/main/java/com/mycompany/webapp/center/controller/CenterController.java
@@ -1,8 +1,17 @@
 package com.mycompany.webapp.center.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;import org.springframework.web.multipart.MultipartFile;
+/**
+ * @ClassName : centerController.java
+ * @Description : 센터에 관한 controller
+ * @Modification 
+ * @
+ * @       수정일                  수정자                 수정내용
+ * @ ==============   ============   ===========
+ * @
+ * @author 이소정
+ * **/
 @Controller
 public class CenterController {
 
@@ -15,5 +24,11 @@ public class CenterController {
 	public String loadCenter() {
 		return "jsp/center/centerList";
 	}
+	
+	@RequestMapping(value="/insertCenter")
+		public String insertCenter() {
+			return "jsp/center/centerinsert";
+	}
+	
 	
 }
--- src/main/java/com/mycompany/webapp/center/service/ICenterService.java
+++ src/main/java/com/mycompany/webapp/center/service/ICenterService.java
@@ -1,5 +1,11 @@
 package com.mycompany.webapp.center.service;
 
 public interface ICenterService {
+	
+	/**
+	 * 센터 정보를 등록한다
+	 * return 센터 등록된 수?
+	 * **/
+	public int insertCenter();
 
 }
