0c664730ab2423474b038ac879c975f586095957
고은별 <rhdmsquf01@naver.com>
2023-01-06 17:56:52 +0900

    1/6 은별

--- src/main/java/com/mycompany/webapp/manager/service/IManagerService.java
+++ src/main/java/com/mycompany/webapp/manager/service/IManagerService.java
@@ -6,15 +6,21 @@ import com.mycompany.webapp.center.vo.CenterVO;
 import com.mycompany.webapp.manager.vo.ManagerVO;
 
 public interface IManagerService {
-	//은별
-	//담당자 등록
+	/* author 고은별
+	 * 담당자 등록 */
 	int insertManager(ManagerVO mgr);
 	
-	//담당자 목록 조회
+	/* author 고은별
+	 * 담당자 목록 조회 */
 	List<ManagerVO> selectManagerList();
 	
-	//담당자 정보 상세조회
+	/* author 고은별
+	 * 담당자 정보 상세조회 */
 	ManagerVO selectManagerDetail(int userCode);
+	
+	/* author 고은별
+	 * 담당자 정보 수정*/
+	int managerUpdate(ManagerVO mgr);
 	
 	//유진
 	List<CenterVO> getCenterByManager(int userCode);
