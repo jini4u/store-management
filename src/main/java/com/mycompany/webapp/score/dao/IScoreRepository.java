4ed013eb314943dc279d1073e96d496691c960fb
정윤선 <jys9522@naver.com>
2023-01-06 15:44:57 +0900

    2023.01.06 윤선 생일 날 insert성공하다.

--- src/main/java/com/mycompany/webapp/score/dao/IScoreRepository.java
+++ src/main/java/com/mycompany/webapp/score/dao/IScoreRepository.java
@@ -6,17 +6,13 @@ import java.util.List;
 import com.mycompany.webapp.score.vo.ScoreVO;
 import java.util.Map;
 
-<<<<<<< HEAD
-import org.apache.ibatis.annotations.Mapper;
 
-@Mapper
-=======
 /**
  * 점수 관리
  * ScoreMapper.xml과 매핑되어있음
  * 
  * */
->>>>>>> branch 'master' of https://github.com/jini4u/store-management.git
+
 public interface IScoreRepository {
 	//유진
 	List<Map<String, String>> getAllGroupCodes();
@@ -32,6 +28,8 @@ public interface IScoreRepository {
 	
 	/*점수 등록*/
 	int insertScore(ScoreVO score);
+	/*모달 점수 리스트 */
+	List<ScoreVO>usingCodeList();
 	/*점수 삭제*/
 	int deleteScore(ScoreVO score);
 	/*점수 수정,저장*/
