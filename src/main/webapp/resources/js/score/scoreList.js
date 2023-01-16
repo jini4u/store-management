27e4567b703a0a31e28bf1d4eb34866b9855ad34
정윤선 <jys9522@naver.com>
2023-01-16 10:19:38 +0900

    2023.01.16

--- src/main/webapp/resources/js/score/scoreList.js
+++ src/main/webapp/resources/js/score/scoreList.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 /**
  * @code.js
@@ -10,23 +9,17 @@
  * 2023.01.13	정윤선
  * @author 정윤선
  * **/
-=======
->>>>>>> branch 'master' of https://github.com/jini4u/store-management.git
+
 window.onload = function(){
    //모달창
    $('#testBtn').click(function(e){
       e.preventDefault();
       $('#testModal').modal("show");
    });
-   
-<<<<<<< HEAD
+
    $("#firstCenter").click(function(){
 	   
 	 alert("점포1입니다.");
-=======
-   $('#firstCenter').click(function(){
-      
-    console.log(result);
->>>>>>> branch 'master' of https://github.com/jini4u/store-management.git
+
    });
 }
