f1fe08aaa199c9c8475e881e0378e1a5799e3463
sojung <red8544@gmail.com>
2023-01-18 21:05:06 +0900

    center POI ..ING -> 2023.01.18 / 21:05 / SOJUNG

--- src/main/java/com/mycompany/webapp/center/service/CenterService.java
+++ src/main/java/com/mycompany/webapp/center/service/CenterService.java
@@ -3,7 +3,10 @@ package com.mycompany.webapp.center.service;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,6 +15,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.mycompany.webapp.center.dao.ICenterRepository;
 import com.mycompany.webapp.center.vo.CenterVO;
+import com.mycompany.webapp.common.dao.IFileRepository;
+import com.mycompany.webapp.common.poi.CenterPOI;
+import com.mycompany.webapp.common.poi.POIClass;
 import com.mycompany.webapp.common.vo.FileInfoVO;
 import com.mycompany.webapp.common.vo.Pager;
 
@@ -20,6 +26,9 @@ public class CenterService implements ICenterService{
 
 	@Autowired
 	ICenterRepository centerRepository;
+	
+	@Autowired
+	IFileRepository fileRepository;
 	
 	@Value("${file.path}")
 	private String filePath;
@@ -197,8 +206,89 @@ public class CenterService implements ICenterService{
 	public int filterCountAllCenters(String keyword) {
 		return centerRepository.filterCountAllCenters(keyword);
 	}
-
 	
-
-
+	@Override
+	public List<Map<String, String>> getCenterUploadHistory() {
+		List<Map<String, Object>> historyList = centerRepository.getCenterUploadHistory();
+		List<Map<String, String>> resultList = new ArrayList<Map<String, String>>();
+		
+		for(Map<String, Object> history:historyList) {
+			String postDate = (String)history.get("postDate");
+			System.out.println("postDate"+postDate);
+			String userName = (String)history.get("userName");
+			String originalName = (String)history.get("originalName");
+			String insert = String.valueOf(history.get("insert"));
+			String update = String.valueOf(history.get("update"));
+			
+			Map<String, String> resultMap = new HashMap<String, String>();
+			resultMap.put("postDate", postDate);
+			resultMap.put("userName", userName);
+			resultMap.put("originalName", originalName);
+			resultMap.put("result", "입력: "+insert+"건, 수정: "+update+"건");
+			
+			resultList.add(resultMap);
+			
+		}
+		return resultList;
+	}
+	
+	@Override
+	public Map<String, Integer> centerUploadFile(MultipartFile file, int startRow) {
+		
+		//리턴할 Map생성
+		Map<String, Integer> resultMap = new HashMap<String, Integer>();
+		resultMap.put("fileNo", 0);
+		resultMap.put("uesrCode", 0);
+		resultMap.put("insert", 0);
+		resultMap.put("update", 0);
+		
+		POIClass poi = new CenterPOI();
+		List<Object> VOList = poi.readWorkBook(file, startRow);
+		for(Object vo : VOList) {
+			CenterVO center = (CenterVO)vo;
+			int exisData = centerRepository.centerDataExist(center);
+			
+			//기존 데이터가 없으면 
+			if (exisData == 0) {
+				centerRepository.insertCenter(center);
+				//replace(첫번쨰는 key, 두번째는 oldValue, 세번째는 newValue) ->저장된 key의 value가 oldvalue
+				//와 동일할 때만 newvalue로 변경, 교체가 되면 true를 리턴, 동일하지 않느면 교체되지 않고 false 리턴
+				resultMap.replace("insert", resultMap.get("insert"), resultMap.get("insert")+1);
+			}else {
+				centerRepository.centerUpdate(center);
+				resultMap.replace("update", resultMap.get("update"), resultMap.get("update")+1);
+			}
+		}
+		String filePathName = filePath+"center_" + file.getOriginalFilename();
+		
+		FileInfoVO fileInfoVO = new FileInfoVO();
+		fileInfoVO.setFileSavedName("center_"+file.getOriginalFilename());
+		fileInfoVO.setOriginalName(file.getOriginalFilename());
+		fileInfoVO.setFileType(file.getContentType());
+		fileInfoVO.setFilePath(filePathName);
+		
+		fileInfoVO.setUploadUserCode(10013);
+		
+		//파일은 왜 저장해야 하지? 히스토리에만 저장해주면 되는거 아닌강? 물어보기
+		//내 추측, history에는 fileno가 있는데 이 fileno를 받아오기 위해 사용하는 건강?
+		fileRepository.insertFile(fileInfoVO);
+		
+		//fileno 왜 필요하지?
+		int fileNo = fileRepository.getFileInfoByOriginalName(fileInfoVO.getOriginalName()).getFileNo();
+		
+		resultMap.replace("fileNo",0, fileNo);
+		resultMap.replace("userCode",0, fileInfoVO.getUploadUserCode());
+		
+		//historyNo는 0일 때 사용하는 코드가 어디에 있는?
+		fileRepository.insertFileUploadHistory(resultMap);
+		
+		try {
+			//이친구는 왜 하는거징? -> 업로드한 파일 데이터를  지정한 파일에 저장한다
+			file.transferTo(new File(filePathName));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return resultMap;
+	}
 }
--- src/main/webapp/WEB-INF/views/jsp/center/excelupload.jsp
+++ src/main/webapp/WEB-INF/views/jsp/center/excelupload.jsp
@@ -1,25 +1,37 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <link rel="stylesheet" href="${pageContext.request.contextPath}/resources/css/score/score.css" />
 <div class="titleBox">
 	<img src="${pageContext.request.contextPath}/resources/images/업로드.png">
 
 	<h2>센터 정보 일괄 등록</h2>
 </div>
+
 <div class='uploadbutton'>
-<input type="text" size="20" placeholder="파일을 선택하세요">
-
-
-<button>파일선택</button>
-<button>업로드</button>
+<form id="file-upload-form" enctype="multipart/form-data" action="centerExcelUpload" method="post">
+<label for="file-select-button" class="greyButton">파일선택</label>
+<input type="file" id="file-select-button" name="centerExcelFile">
+<input type="submit" id="file-upload-button" class="pinkButton">
+</form>
+<!-- <button id="file-upload-button" class="pinkButton">업로드</button> -->
 </div>
 
 
 <table class="scoretable">
+<tr>
 	<th>업로드 날짜</th>
 	<th>업로드 한 담당자</th>
 	<th>파일명</th>
 	<th>구분</th>
-
+</tr>
+	<c:forEach items="${historyMapList}" var="historyMap">
+		<tr>
+			<td>${historyMap.postDate}</td>
+			<td>${historyMap.userName}</td>
+			<td>${historyMap.originalName}</td>
+			<td>${historyMap.result}</td>
+		</tr>
+	</c:forEach>
 
 </table>
